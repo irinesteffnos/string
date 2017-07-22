301357ce0933d444cfb617a51b0bac4adfc0e5a8
hp <hp@DESKTOP-SJQLIE4>
2017-07-22 14:30:42 -0700

    Basic string concept

---
+++ src/com/string/Value.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class Value {
+	public static void main(String args[]){  
+		int value=30;  
+		String s1=String.valueOf(value);  
+		System.out.println(s1+10);//concatenating string with 10  
+		}
+}
